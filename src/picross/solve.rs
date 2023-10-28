68f9785d29d7627a47b7615ac02bfbc80866b352
PicrossSoldier <94438164+PicrossSoldier@users.noreply.github.com>
2023-10-28 19:43:25 +0900

    feat: ✨ プライオリティの実装や想定していたレイヤー構造の変更に基づく、処理フローの変更

--- src/picross/solve.rs
+++ src/picross/solve.rs
@@ -11,152 +11,82 @@ enum Direction {
     Column,
 }
 
-impl Direction {
-    fn switch(&mut self) {
-        match self {
-            Direction::Row => *self = Direction::Column,
-            Direction::Column => *self = Direction::Row,
-        }
-    }
-}
-
-struct Position {
-    row_index: usize,
-    column_index: usize,
-}
-
-impl Position {
-    fn new(row_index: usize, column_index: usize) -> Self {
-        Self {
-            row_index,
-            column_index,
-        }
-    }
-}
-
-struct Layer {
-    branch_position: Position,
-    held_grid: Option<Grid>,
-}
-
-impl Layer {
-    fn new(branch_position: Position) -> Self {
-        Self {
-            branch_position,
-            held_grid: None,
-        }
-    }
-
-    fn hold(&mut self, grid: Grid) {
-        self.held_grid = Some(grid);
-    }
-}
-
-struct LayerStack {
-    vector: Vec<Layer>,
-}
-
-impl LayerStack {
-    fn new() -> Self {
-        Self { vector: Vec::new() }
-    }
-
-    fn get_num(&self) -> usize {
-        self.vector.len()
-    }
+struct LineID {
+    direction: Direction,
+    index: usize,
 }
 
 struct Solver<'a> {
     board_grid: &'a mut Grid,
-    solver_grid: Grid,
-    layer_stack: LayerStack,
-    direction: Direction,
 }
 
 impl<'a> Solver<'a> {
     pub fn new(board: &'a mut Board) -> Self {
-        let solver_grid = Grid::new(board.grid.row_num, board.grid.column_num);
-        let layer_stack = LayerStack::new();
-
         Self {
             board_grid: &mut board.grid,
-            solver_grid,
-            layer_stack,
-            direction: Direction::Row,
         }
     }
+}
 
-    fn init(&mut self) {
-        self.solver_grid = Grid::new(self.board_grid.row_num, self.board_grid.column_num);
-        self.layer_stack = LayerStack::new();
-        self.direction = Direction::Row;
-    }
+impl Solver<'_> {
+    fn init(&mut self) {}
 
     fn is_base_layer(&self) -> bool {
-        if self.layer_stack.get_num() == 0 {
-            true
-        } else {
-            false
-        }
-    }
-
-    fn is_changed(&self) -> bool {
         true
     }
 
-    fn is_full_grid(&self) -> bool {
+    fn has_full_grid(&self) -> bool {
         true
     }
 
-    fn is_full_grid_held(&self) -> bool {
+    fn apply_line(&mut self, line_id: LineID) -> bool {
         true
     }
 
-    fn solve_direction(&mut self) -> bool {
+    fn stack_layer(&mut self) -> bool {
         true
     }
 
-    fn direction_switch(&mut self) {
-        self.direction.switch();
+    fn priority_pop(&mut self) -> Option<LineID> {
+        Some(LineID {
+            direction: Direction::Row,
+            index: 0,
+        })
     }
 
-    fn peel_off(&mut self) {}
+    fn apply_opposite_layer(&mut self) {}
 
     fn remove_stacked_layers(&mut self) {}
 
-    fn hold(&mut self) {}
+    fn set_full_grid(&mut self) {}
 
-    fn stack(&mut self) {}
+    fn switch_layer(&mut self) {}
 
     pub fn solve(&mut self) -> SolveResult {
         self.init();
-
         loop {
-            if self.is_changed() {
-                if self.solve_direction() {
-                    self.direction_switch();
-                } else {
+            if let Some(line_id) = self.priority_pop() {
+                if !self.apply_line(line_id) {
                     if self.is_base_layer() {
                         self.init();
                         break SolveResult::NoAnswer;
                     } else {
-                        self.peel_off();
+                        self.apply_opposite_layer();
                     }
                 }
             } else {
-                if self.is_full_grid() {
+                if !self.stack_layer() {
                     if self.is_base_layer() {
                         break SolveResult::OnlyOneAnswer;
                     } else {
-                        if self.is_full_grid_held() {
+                        if self.has_full_grid() {
                             self.remove_stacked_layers();
                             break SolveResult::NoFixedAnswer;
                         } else {
-                            self.hold();
+                            self.set_full_grid();
+                            self.switch_layer();
                         }
                     }
-                } else {
-                    self.stack();
                 }
             }
         }
